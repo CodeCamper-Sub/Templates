21db4a7c43e45a7bf27bd1865c02382bc108ea11
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2023-08-01 11:52:57 +0900

    SwiftUI Template

--- File Templates/API.xctemplate/___FILEBASENAME___.swift
+++ File Templates/API.xctemplate/___FILEBASENAME___.swift
@@ -1,25 +1,44 @@
 //___FILEHEADER___
 
 import Moya
+import UIKit
 
-
-enum ___VARIABLE_productName___ {
-  // case login(LoginRequest)
-}
-
-extension ___VARIABLE_productName___: SugarTargetType {
-  var route: Route {
+enum ___FILEBASENAME___: ApiTargetType {
+  // case login(data: LoginRequest)
+  
+  var path: String {
     switch self {
 //    case .login:
-//      return .post("/auth")
+//      return "/auth"
+    }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+//    case .login:
+//      return .get
     }
   }
   
   var task: Task {
     switch self {
+//    case let .login(data):
+//      return .requestCustomJSONEncodable(data, encoder: .ISO8601)
+//      return .requestParameters(parameters: try! data.asDictionary(), encoding: URLEncoding.default)
       
     default:
       return .requestPlain
     }
   }
 }
+
+extension ___FILEBASENAME___ {
+  var sampleData: Data {
+    switch self {
+    default:
+      return Data()
+//      return NSDataAsset(name: "GetClassesResponse")!.data
+    }
+  }
+}
+
---
+++ File Templates/ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+//___FILEHEADER___
+
+import Foundation
+import SwiftUI
+import Combine
+
+@MainActor
+class ___FILEBASENAME___: ObservableObject {
+  var subscriptions = [AnyCancellable]()
+  
+  init() {
+    
+    bind()
+  }
+  
+  func bind() {
+    
+  }
+}
