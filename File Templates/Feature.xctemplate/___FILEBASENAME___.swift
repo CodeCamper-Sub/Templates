eecdc1f66f7789a5530af8d56c7e8ccf3827be6a
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2024-05-10 21:40:13 +0900

    TCA - Feature Template 추가

---
+++ File Templates/Feature.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,27 @@
+//___FILEHEADER___
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct ___FILEBASENAME___ {
+  enum Action {
+    
+  }
+  
+  @ObservableState
+  struct State: Equatable {
+    
+    
+  }
+  
+  var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+
+      }
+      
+      return .none
+    }
+  }
+}
